5ee4a08cb91cb0e86312f886b36f909e8f090dbe
davidcoats <david@getsafetycone.com>
2024-01-16 12:22:06 -0800

    Initial commit.

---
+++ source/R5T.T0236/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0236
+{
+	/// <summary>
+	/// Catch-all for IHasX/IWithX strictly-framework types with no associated functionality. (netstandard2.1)
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.T0236/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.T0236
+Catch-all for IHasX/IWithX strictly-framework types with no associated functionality. (netstandard2.1)
